3311f019085ff88388d690d0887eb2f6206022b0
micberts <micaela.reutersward@gmail.com>
2024-03-14 10:22:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function buildmenu()
+{
+    var links = [
+["Startsida", "index.html"],
+["Receptförslag", "recept.html"],
+["Samarbeten", "samarbeten.html"],
+["Media", "media.html"]
+]
+
+var content = ""
+for(var i = 0; i < links.length; i++)
+{
+var text = links [i][0]
+var adress = links [i][1]
+content = content + "<li><a href=\"" + adress + "\">" + text + "</a></li>"
+//<li><a href="index.html">Startsidan</a></li>
+}
+document.getElementById("menu").innerHTML = content
+
+}
+
