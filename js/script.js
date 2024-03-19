d7ee3aba49a8a405f6d457d14c8b7fa83fd944cb
micberts <micaela.reutersward@gmail.com>
2024-03-19 14:30:59 +0100

    commit number 3

--- js/script.js
+++ js/script.js
@@ -4,18 +4,20 @@ function buildmenu()
 ["Startsida", "index.html"],
 ["Receptförslag", "recept.html"],
 ["Samarbeten", "samarbeten.html"],
-["Media", "media.html"]
-]
+["Media", "media.html"],
+["Nyhetsbrev", "https://micaelareutersward2.wordpress.com/"]
+];
 
 var content = ""
 for(var i = 0; i < links.length; i++)
 {
-var text = links [i][0]
-var adress = links [i][1]
-content = content + "<li><a href=\"" + adress + "\">" + text + "</a></li>"
+var text = links [i][0];
+var adress = links [i][1];
+content = content + "<li><a href=\"" + adress + "\">" + text + "</a></li>";
 //<li><a href="index.html">Startsidan</a></li>
 }
-document.getElementById("menu").innerHTML = content
+
+document.getElementById("menu").innerHTML = content;
 
 }
 
